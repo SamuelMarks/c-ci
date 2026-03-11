33674237487654b935f6547a4a5e9f8c9128ba90
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-03-11 22:00:38 +1300

    Fix string quoting in ctest arguments

--- build_msvc2005.cmd
+++ build_msvc2005.cmd
@@ -23,7 +23,7 @@ cmake --build "%BUILD_DIR%" --config "%BUILD_TYPE%"
 if errorlevel 1 exit /b 1
 pushd "%BUILD_DIR%"
 set PATH=%BUILD_DIR%\%BUILD_TYPE%;%BUILD_DIR%\_deps\c89stringutils-build\%BUILD_TYPE%;%BUILD_DIR%\_deps\c_abstract_http-build\%BUILD_TYPE%;%PATH%
-ctest -C \x22%BUILD_TYPE%\x22 --output-on-failure
+ctest -C "%BUILD_TYPE%" --output-on-failure
 if errorlevel 1 exit /b 1
 popd
 
